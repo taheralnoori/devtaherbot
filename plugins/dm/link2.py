c822db436d14167c64b5bc81d3749ca2be0bc361
taheralnoori <82815443+taheralnoori@users.noreply.github.com>
2022-05-05 18:37:16 +0300

    Update link2.py

--- plugins/dm/link2.py
+++ plugins/dm/link2.py
@@ -2,7 +2,6 @@ import os
 import requests
 import weasyprint
 import urllib.request
-from presets import Presets
 from bs4 import BeautifulSoup
 from fpdf import FPDF
 from pdf import PROCESS
@@ -18,7 +17,7 @@ from pyrogram.types import InlineKeyboardMarkup
 async def link_extract(self, m: Message):
     if not m.text.startswith("http"):
         await m.reply_text(
-            Presets.INVALID_LINK_TXT,
+            "<b>رابط غير صحيح (Invalid link) </b>\n\n<i>من فضلك أرسل لي رابط صحيح (Please send me a valid link)</i>",
             reply_to_message_id=m.message_id,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Close", callback_data="close_btn")]]
@@ -30,14 +29,14 @@ async def link_extract(self, m: Message):
     thumb_path = os.path.join(os.getcwd(), "img")
     if not os.path.isdir(thumb_path):
         os.makedirs(thumb_path)
-        urllib.request.urlretrieve(Presets.THUMB_URL, os.path.join(thumb_path, "thumbnail.png"))
+        urllib.request.urlretrieve("https://telegra.ph/file/60706bd59c0829ed2f76f.jpg", os.path.join(thumb_path, "thumbnail.png"))
     else:
         pass
     #
     thumbnail = os.path.join(os.getcwd(), "img", "thumbnail.png")
     #
     await self.send_chat_action(m.chat.id, "typing")
-    msg = await m.reply_text(Presets.PROCESS_TXT, reply_to_message_id=m.message_id)
+    msg = await m.reply_text("<code>جارٍ معالجة الرابط (Processing the link)..⏳</code>", reply_to_message_id=m.message_id)
     try:
         req = requests.get(m.text)
         # using the BeautifulSoup module
@@ -49,20 +48,20 @@ async def link_extract(self, m: Message):
         weasyprint.HTML(m.text).write_pdf(file_name)
     except Exception:
         await msg.edit_text(
-            Presets.ERROR_TXT,
+            "<b>خطأ URL(Error)</b>\n\n<i>تعذر إنشاء ملف PDF باستخدام عنوان URL هذا.(Unable to create a Pdf with this URL.)\n(Try again with a valid one..(حاول مرة أخرى باستخدام صالح ..)</i>",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Close", callback_data="close_btn")]]
             )
         )
         return
     try:
-        await msg.edit(Presets.UPLOAD_TXT)
+        await msg.edit("<b>تحميل ملفك (Uploading your file..)🔋</b>")
     except Exception:
         pass
     await self.send_chat_action(m.chat.id, "upload_document")
     await m.reply_document(
         document=file_name,
-        caption=Presets.CAPTION_TXT.format(file_name),
+        caption="{}\n\n<b>بواسطة(by):</b><a href='https://t.me/i2pdfbot'><b> Dev: @ta_ja199</b></a>".format(file_name),
         thumb=thumbnail
     )
     print(
