e51f778d286f502d0f11210fa1382f7b30b4e707
taheralnoori <82815443+taheralnoori@users.noreply.github.com>
2022-05-05 19:20:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
